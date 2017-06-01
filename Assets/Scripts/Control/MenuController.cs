0403344bd82d9e59ab717e7a66cf414d1c60f13e
angelomatni <angelomatni2018@u.northwestern.edu>
2017-06-01 00:31:35 -0500

    Made a main menu.

---
+++ Assets/Scripts/Control/MenuController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+
+	public GameObject menu;
+	int activeMenu;
+
+	void Awake() {
+		activeMenu = 0;
+	}
+
+	public void LoadMenuPage(int menuIndex) {
+		menu.transform.GetChild (activeMenu).gameObject.SetActive (false);
+		activeMenu = menuIndex;
+		menu.transform.GetChild (activeMenu).gameObject.SetActive (true);
+	}
+
+	public void ExitGame() {
+		Application.Quit ();
+	}
+
+	public void LoadScene(int index) {
+		SceneManager.LoadScene (GameController.GetSceneName (index));
+	}
+}
